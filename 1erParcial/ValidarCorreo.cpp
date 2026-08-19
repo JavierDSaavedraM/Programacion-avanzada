d35a5d22ee789095bf62703b2ac128563f07fac1
SaZ03 <134658292+SaZ03@users.noreply.github.com>
2026-08-19 15:39:04 -0600

    Update ValidarCorreo.cpp

--- 1erParcial/ValidarCorreo.cpp
+++ 1erParcial/ValidarCorreo.cpp
@@ -6,37 +6,37 @@ using namespace std;
 
 bool Validar_Usuario(string usuario){
     if (usuario.size()<1){
-        cout << "El usuario cuenta con muy pocos caracteres, incorrecto" << endl;
+        cout << "El usuario '"<< usuario << "' cuenta con muy pocos caracteres, incorrecto" << endl;
         return false;
     }
     
     if (usuario.size() > 20){
-        cout << "El usuario cuenta con muchos caracteres, incorrecto" << endl;
+        cout << "El usuario '"<< usuario << "' cuenta con muchos caracteres, incorrecto" << endl;
         return false;
     }
     regex pattern(R"([^\da-z_])");
     smatch match;
     if (regex_search(usuario,match,pattern))
     {  
-        cout << "Error, el usuario contiene el siguiente caracter, no es valido " << match.str() << endl;
+        cout << "Error, el usuario '"<< usuario << "' contiene el siguiente caracter, no es valido " << match.str() << endl;
         return false;
     }
     pattern = regex(R"(^\d)");
     if (regex_search(usuario,match,pattern))
     {  
-        cout << "Error, el usuario debe de comenzar con una letra minuscula" << endl;
+        cout << "Error, el usuario '"<< usuario << "' debe de comenzar con una letra minuscula" << endl;
         return false;
     }
     pattern = regex(R"(_$)");
     if (regex_search(usuario,match,pattern))
     {  
-        cout << "Error, el usuario no puede terminar con un guion bajo" << endl;
+        cout << "Error, el usuario '"<< usuario << "' no puede terminar con un guion bajo" << endl;
         return false;
     }
     pattern = regex(R"(__)");
     if (regex_search(usuario,match,pattern))
     {  
-        cout << "Error, el usuario no puede contener dos guiones bajos consecutivos" << endl;
+        cout << "Error, el usuario '"<< usuario << "' no puede contener dos guiones bajos consecutivos" << endl;
         return false;
     }
 
@@ -48,28 +48,28 @@ bool Validar_Dominio(string dominio){
     smatch match;
     if (regex_search(dominio,match,pattern))
     {  
-        cout << "Error, el dominio no puede contener letras mayusculas" << endl;
+        cout << "Error, el dominio '"<< dominio << "' no puede contener letras mayusculas" << endl;
         return false;
     }
     
     pattern = regex(R"([^a-z.])");
     if (regex_search(dominio,match,pattern))
     {  
-        cout << "Error, el dominio no puede contener un caracter que no sea una letra minuscula, un numero o un punto" << endl;
+        cout << "Error, el dominio '"<< dominio << "' no puede contener un caracter que no sea una letra minuscula, un numero o un punto" << endl;
         return false;
     }
 
     pattern = regex(R"(\..*\.)");
     if (regex_search(dominio,match,pattern))
     {  
-        cout << "Error, el dominio no puede contener mas de 1 punto" << endl;
+        cout << "Error, el dominio '"<< dominio << "' no puede contener mas de 1 punto" << endl;
         return false;
     }
 
     pattern = regex(R"(^\.|\.$)");
     if (regex_search(dominio,match,pattern))
     {  
-        cout << "Error, en el dominio el punto no puede ser el primer ni el último carácter del dominio" << endl;
+        cout << "Error, el dominio '"<< dominio << "' el punto no puede ser el primer ni el último carácter del dominio" << endl;
         return false;
     }
     return true;
@@ -79,6 +79,9 @@ int main(){
     vector<string> candidatos1 = {"juan_perez@mail.com", "1ana@mail.com", "luis__m@tech.net", "maria@.com", "carlos_@domain.org", "z@a.b"};
     vector<string> candidatos2 = {"admin@sys.com", "root@sys.com", "test@otro.dominio.com", "admin@sys..com"};
     vector<string> candidatos3 = {"@sinusuario.com", "usuario_largo_que_pasa_de_veinte_caracteres@mail.com", "user@tech.", "us3r_n4me@domain.mx"};
+    
+    vector<string> correctos;
+    cout << "Correos con errores: " << endl;
     for (int i = 0; i < candidatos1.size(); i++) {
         string correo = candidatos1[i];
         int ubicacion = correo.find('@');
@@ -89,8 +92,14 @@ int main(){
         string dominio = correo.substr(ubicacion + 1);
         if (Validar_Usuario(usuario) == true and Validar_Dominio(dominio) == true)
         {
-            cout << "El correo es valido" << endl;
+            correctos.push_back(candidatos1[i]);
         }
+    }
+    
+    cout << "\nCorreos correctos: " << endl;
+    for (int i = 0; i < correctos.size(); i++)
+    {
+        cout << correctos[i] << endl;
     }
     
 
