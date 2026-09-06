c520978463e3d6d31fd537af751d22fb21c86ae8
JavierD <javier.saavedram@udem.edu>
2026-09-06 08:34:49 -0600

    completar codigo

--- 1erParcial/examen.cpp
+++ 1erParcial/examen.cpp
@@ -15,12 +15,23 @@ Damos nuestra palabra que hemos realizado esta actividad con integridad académi
 #include <algorithm>
 #include <cmath>
 #include <chrono>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std; 
 
-bool calculo(string &tablero){
+// COUT PARA DEBUG
+#ifdef DEBUG
+#define LOG_DEBUG(msg) cout << "[DEBUG]" << msg << endl;
+#else
+#define LOG_DEBUG(msg)
+#endif
+
+// Funcion que obtiene un string  
+string eliminar_combo(string _tablero){
+    string tablero = _tablero;
     bool cambio = false;
-    if (tablero.empty()) return false;
+    if (tablero.empty()) return "";
     char color = tablero[0];
     int contador = 1;
     for (size_t i = 1; i < tablero.size(); i++) {
@@ -42,65 +53,87 @@ bool calculo(string &tablero){
             cambio = true;
         }
     }
-    return cambio;
+    return tablero;
 }
 
+vector<size_t> obtenerPosValidas(string _t, string _c){
+    string tablero_actual = _t;
+    string color_actual = _c;
 
-int juego(string& tablero,string& mano){
-    if (tablero.empty()) return 1;
-    if (mano.empty()) return 0;
-    
-    for (size_t i = 0; i < mano.size(); i++) {
-        for (size_t j = 0; j < tablero.size(); j++) {
-            string tableroAct = tablero;
-            string manoAct = mano;
+    vector<size_t> pos_validas;
+    unordered_set<string> valores_anteriores;
 
-            tableroAct.insert(j,1,manoAct[i]);
-            manoAct.erase(i,1);
-            calculo(tableroAct);
-            
-            if (juego(tableroAct,manoAct) == 1) {
-                return 1;
+    for (size_t pos = 0; pos <= tablero_actual.size(); pos++){
+        string nuevo_tablero = tablero_actual;
+        nuevo_tablero.insert(pos, color_actual);
+
+        if(valores_anteriores.find(nuevo_tablero) == valores_anteriores.end()){
+            valores_anteriores.insert(nuevo_tablero);
+            pos_validas.push_back(pos);
+        }
+    }
+
+    return pos_validas;
+}
+
+// Sirve para memoizar los estados ya visitados y prunar ramas inecesarias.
+unordered_map<string, int> estados_visitados;
+// Funcion recursiva que genera todos los posibles inserciones
+// @input: toma como parametros una copia del tablero (_t) y de la mano (_m)
+// @output: regresa el numero maximo de colores restantes
+int resolver(string _t, string _m){
+    string tablero = eliminar_combo(_t);
+    string mano = _m;
+    sort(mano.begin(), mano.end());
+    if(tablero.empty()) return mano.size();
+    if(mano.empty()) return -1;
+    string estado_actual = tablero + "#" + mano;
+    if(estados_visitados.find(estado_actual) != estados_visitados.end()){
+        return estados_visitados[estado_actual]; 
+    }
+    int mejor_mano = -1;
+    for (size_t color = 0 ; color < mano.size() ; color++){
+        string color_actual;
+        color_actual.push_back(mano[color]);
+        if(color > 0 && mano[color] == mano[color-1]){
+            continue;
+        }
+        vector<size_t> pos_validas = obtenerPosValidas(tablero, color_actual);
+        for (size_t espacio_tablero = 0; espacio_tablero < pos_validas.size(); espacio_tablero++){
+            string nuevo_tablero = tablero;
+            string nueva_mano = mano;
+            nuevo_tablero.insert(pos_validas[espacio_tablero], color_actual);
+            nuevo_tablero = eliminar_combo(nuevo_tablero);
+            nueva_mano.erase(color, 1);
+            int resultado_hojas = resolver(nuevo_tablero, nueva_mano);
+            LOG_DEBUG(estado_actual << "/" << nuevo_tablero << "#" << nueva_mano << "/" << resultado_hojas);
+            if (resultado_hojas != -1){
+                mejor_mano = max(mejor_mano, resultado_hojas);
             }
         }
     }
+    estados_visitados[estado_actual] = mejor_mano;
+    return mejor_mano;
 }
 
 //voy revisando cada color del tablero y hago 2 cosas, o le meto uno de ese color o no 
 
-int juego2(string& tablero,string& mano){
-    if (tablero.empty()) return mano.size();
-    if (mano.empty()) return 9999;
-    
-    int mejorCaso = 9999; 
-    for (size_t i = 0; i < tablero.size(); i++) {
-        for (size_t j = 0; j < mano.size(); j++) {
-            if (mano[j] == tablero[i]) {
-                // pruebo si lo meto y no 
-                string tableroAct = tablero;
-                string manoAct = mano;
-
-                tableroAct.insert(i,1,manoAct[j]);
-                manoAct.erase(j,1);
-                calculo(tableroAct);
-
-                int intento = juego2(tableroAct,manoAct);
-                if (intento > mejorCaso) {
-                    mejorCaso = intento;
-                }
-            }
-        }
-    }
-}
-
 int main(){
     vector<string> tableros{"rr","rg","rbb","wwrrbbww","bbyyrrbb","bbwwrrwwbb","wgrrbbyyw","rybwg","rrwwbbyyggrr","rrwybwrr"};  
     vector<string> manos{"r","rrgg","rrbb","wrbrw","byr","wrb","wbgry","rryybbwwgg","gybwgybw","wwyybbw"};     
-    
-    for (int i = 0; i < tableros.size(); i++) {
-        cout << "tablero numero " << i+1 << " :" << endl;
-        cout << "numero de colores restantes: " << juego2(tableros[i],manos[i]) << endl;
+
+    string board = tableros.at(4);
+    string hand = manos.at(4);
+    cout << board << "/"<< hand << " " << 
+        resolver(board, hand) << endl;
+    /*
+    for(size_t i= 0; i < tableros.size(); i++){
+        string board = tableros.at(i);
+        string hand = manos.at(i);
+        cout << board << "/"<< hand << " " << 
+            resolver(board, hand) << endl;
     }
+    */
 
     return 0;
 }
